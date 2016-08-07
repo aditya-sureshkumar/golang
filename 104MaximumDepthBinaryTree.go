b3d78c5c66643f2e0f5c8c81961f4a0a3ccb56ed
aditya-sureshkumar <adisk93@gmail.com>
2016-08-06 19:35:52 -0700

    leetcode1

---
+++ 104MaximumDepthBinaryTree.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+    if root == nil{
+        return 0
+    }
+    if root.Left==nil && root.Right==nil {
+        return 1
+    }
+    return int(math.Max(float64(maxDepth(root.Left)),float64(maxDepth(root.Right)))+1)
+}
